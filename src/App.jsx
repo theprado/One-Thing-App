499bc4bd66b6670cc02cccaa14d80ab4f68caa3b
Matheus do Prado <32469329+theprado@users.noreply.github.com>
2023-01-23 18:09:53 -0300

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { ArrowRightCircleIcon, BeakerIcon } from '@heroicons/react/24/solid'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-
-    </div>
+    <main className="grid place-items-center min-h-screen bg-gradient-to-b from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900 text-slate-900 dark:text-slate-200">
+        <div className="grid place-items-center gap-8 m-8">
+        <h1 className='text-3xl sm:text-6xl font-bold text-center'>Oque você precisa fazer?</h1>
+        <form
+          className='flex ring-4 rounded-md ring-slate-200 dark:ring-slate-800 focus-within:ring-teal-600 focus-within:ring-offset-4 bg-slate-200 ring-offset-slate-200 dark:ring-offset-slate-800'
+      //    onSubmit={}
+    >
+      <input type='text-slate-900'
+        className='w-[500px]'/>
+      <button className=''>
+        <ArrowRightCircleIcon className='h-12 w-12 dark:text-slate-800'/>
+      </button>
+        </form>
+        </div>
+    </main>
+  
   )
 }
 
